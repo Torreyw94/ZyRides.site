e1daf7f7fca422e19931a8d7f752e3917ddb0a21
Torreyw94 <145612792+Torreyw94@users.noreply.github.com>
2024-10-21 18:26:17 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_RlyGSijKw
+        userPoolClientId: '', // e.g. 4bctjcm8rk5sbtcj194pfgnksd
+
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
